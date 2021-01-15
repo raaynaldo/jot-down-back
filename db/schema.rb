efe278459dc2e55f2a15e9b016bc8e9a147eae03
raaynaldo <raaynaldo@hotmail.com>
2021-01-15 15:56:53 -0500

    update datatype for body

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_210510) do
+ActiveRecord::Schema.define(version: 2021_01_15_205455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2021_01_13_210510) do
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
     t.datetime "last_updated_at"
     t.boolean "archived"
     t.boolean "deleted"
